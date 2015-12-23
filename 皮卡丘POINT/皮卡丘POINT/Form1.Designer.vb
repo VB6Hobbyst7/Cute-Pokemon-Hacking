4e475dfe3594b0b4fa930034c62981e5087fb9da
unromanticman <umworksite@gmail.com>
2015-12-24 02:33:34 +0800

    Patch Like A HACK

---
+++ 皮卡丘POINT/皮卡丘POINT/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 3000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(106, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(108, 22)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "10"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 126)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(332, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(53, 17)
+        Me.ToolStripStatusLabel2.Text = "偵測中..."
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(159, 17)
+        Me.ToolStripStatusLabel1.Text = "尚未發現可愛的皮卡丘們T_T"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(89, 16)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Score Control"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(237, 105)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(83, 12)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Powered By UM"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(229, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 12)
+        Me.Label1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(106, 78)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(108, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 12)
+        Me.Label2.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "左邊玩家"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "右邊玩家"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(332, 148)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "皮卡丘打排球小外掛"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+    Public WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ 皮卡丘POINT/皮卡丘POINT/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    '=====================
+    Sub readPointer()
+        On Error Resume Next
+        While True
+            If ((Process.GetProcessesByName("皮卡丘打排球").Length <> 0)) Then
+                 Label1.Text = ReadDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H40}, 3)
+                Label2.Text = ReadDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H3C}, 3)
+
+                ToolStripStatusLabel1.Text = "捕獲野生的皮卡丘!!XD"
+            Else
+
+                ToolStripStatusLabel1.Text = "尚未發現可愛的皮卡丘們T_T"
+            End If
+
+            Threading.Thread.Sleep(3000)
+        End While
+    End Sub
+
+    Sub writePointer()
+        On Error Resume Next
+        While True
+            If ((Process.GetProcessesByName("皮卡丘打排球").Length <> 0)) Then
+                WriteDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H40}, TextBox1.Text, 3)
+                WriteDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H3C}, TextBox2.Text, 3)
+                
+                ToolStripStatusLabel1.Text = "捕獲野生的皮卡丘!!XD"
+            Else
+
+                ToolStripStatusLabel1.Text = "尚未發現可愛的皮卡丘們T_T"
+            End If
+
+            Threading.Thread.Sleep(3000)
+        End While
+    End Sub
+    '=====================
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If ((Process.GetProcessesByName("皮卡丘打排球").Length <> 0)) Then
+                WriteDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H40}, TextBox1.Text, 3)
+                WriteDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H3C}, TextBox2.Text, 3)
+                Label1.Text = ReadDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H40}, 3)
+                Label2.Text = ReadDMAInteger("皮卡丘打排球", &H411F28, {&H28, &H10, &H3C}, 3)
+
+                ToolStripStatusLabel1.Text = "捕獲野生的皮卡丘!!XD"
+            Else
+
+                ToolStripStatusLabel1.Text = "尚未發現可愛的皮卡丘們T_T"
+            End If
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form.CheckForIllegalCrossThreadCalls = False
+
+        Try
+            writePointerT.Start()
+
+            readPointerT.Start()
+        Catch ex As Exception
+
+        End Try
+      
+    End Sub
+    Dim writePointerT As New Threading.Thread(AddressOf writePointer)
+    Dim readPointerT As New Threading.Thread(AddressOf readPointer)
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("http://umworksite.blogspot.tw/")
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.Checked = True) Then
+            'Timer1.Enabled = True
+            writePointerT.Start()
+        Else
+            writePointerT.Suspend()
+        End If
+    End Sub
+End Class
